e43543a352a1eb3c5bbfd7fcc8d32e180acb872b
darkcount2011 <pedrotje.b@gmail.com>
2021-01-18 12:54:55 +0100

    finals

--- src/objectsPrivacy/BlindsPicture.ts
+++ src/objectsPrivacy/BlindsPicture.ts
@@ -4,7 +4,7 @@ class blindsPicture extends GameObjects {
 
 
     constructor (xPos: number, yPos: number, thisWidth:number, thisHeight:number){
-        super("blindsPicture","./assets/imgPrivacy/BlindsPicture.png", xPos, yPos);
+        super("blindsPicture","./assets/imgPrivacy/blindsPicture.png", xPos, yPos);
         this.image.width = thisWidth;
         this.image.height = thisHeight;
         }
